3ab72d16b29d647a6e4621a9f2c833668a147175
ignacioct <ignaciotalaveracepeda@gmail.com>
2022-02-25 20:40:05 +0100

    dataframe and diversity chart

---
+++ app.py
@@ -0,0 +1,77 @@
+"""
+Stock Dashboard App. 
+
+Visualization of portfolios. Made with Python 3.7 and Streamlit.
+Ignacio Talavera Cepeda, 2022.
+"""
+
+from csv_parser import Portfolio
+
+import matplotlib.pyplot as plt
+import matplotlib
+import pandas as pd
+import seaborn as sns
+import streamlit as st
+
+matplotlib.rcParams['axes.grid'] = True
+matplotlib.rcParams['savefig.transparent'] = True
+
+def donughtDiversity(portfolio: pd.DataFrame) -> None:
+    """
+    Creating a donught chart with the diversity of the Portfolio.
+
+    Args:
+        portfolio: Pandas dataframe with the contents of the csv and some postprocessing data.
+    """
+
+    # Define Seaborn color palette to use
+    # When there are more than 5 stocks, this might go a little crazy
+    colors = sns.color_palette('pastel')[0:5]
+
+    # Define Data and labels
+    data = []
+    labels = []
+
+    for _, row in portfolio.iterrows():
+        data.append(row["Amount"])
+        labels.append(row["Stock"])
+    
+    # Pie chart in which the donught chart is based
+    plt.pie(data, labels = labels, colors = colors, autopct='%.0f%%')
+
+    # add a circle at the center to transform it in a donut chart
+    my_circle=plt.Circle( (0,0), 0.7, color='white')
+    p=plt.gcf()
+    p.gca().add_artist(my_circle)
+
+    plt.show()
+
+
+def main() -> None:
+
+    st.set_page_config(layout="wide")
+    
+    # Load the portfolio
+    portfolio = Portfolio("portfolio.csv")
+
+    # Header
+
+    st.markdown("<h1 style='text-align: center;'>Stock Portfolio Dashboard</h1>", unsafe_allow_html=True)
+    st.markdown("<h4 style='text-align: center;'>A Web App by <a href=https://github.com/ignacioct>Ignacio Talavera</a></h4>", unsafe_allow_html=True)
+
+    # State of Portfolio and Diversity share row
+    firstCol, _, secondCol = st.columns((2.5, 0.75, 1.5))
+
+    # Show dataframe of own stocks
+    firstCol.header("Current State of Portfolio")
+    firstCol.dataframe(portfolio.df)
+
+    # Show pie chart with portfolio diversity
+    secondCol.header("Portfolio Diversity")
+
+    fig = plt.figure()
+    donughtDiversity(portfolio.df)
+    secondCol.pyplot(fig)
+
+if __name__ == "__main__":
+    main()
---
+++ csv_parser.py
@@ -0,0 +1,46 @@
+"""
+Class in charge of parsing the contents of the CSV file.
+The data is saved on a Pandas Dataframe.
+"""
+
+import pandas as pd
+from yahoo_fin import stock_info as si
+
+class Portfolio:
+
+    def __init__(self, path) -> None:
+        
+        self.csvPath = path
+
+        # Load basic DF
+        self.df = pd.read_csv(self.csvPath)
+
+        # Adding extra columns
+        moneyInvestedList = []
+        currentPriceList = []
+        currentValueInvestmentList = []
+        gainLossList = []
+        positiveList = []
+        
+        for _, row in self.df.iterrows():
+            
+            # Obtaining extra variables
+            moneyInvested = row["Price"]*row["Amount"]
+            currentPrice = si.get_live_price(row["Stock"].lower())
+            gainLoss = (currentPrice - row["Price"]) *100 / row["Price"]
+            currentValueInvestment = (currentPrice*row["Amount"]) - (row["Price"]*row["Amount"])
+            positive = True if gainLoss > 0 else False
+
+            # Adding them to lists
+            moneyInvestedList.append(moneyInvested)
+            currentPriceList.append(currentPrice)
+            currentValueInvestmentList.append(currentValueInvestment)
+            positiveList.append(positive)
+            gainLossList.append(gainLoss)
+
+        # Adding new lists as columns of the DataFrame
+        self.df["Money Invested"] = moneyInvestedList
+        self.df["Current Price"] = currentPriceList
+        self.df["Gain/Loss %"] = gainLossList
+        self.df["Current Value Investment"] = currentValueInvestmentList
+        
